1e94a1b419e2f6581c2c91996480349054bffeb5
Brnalxz <alexander.brandon425@gmail.com>
2024-09-26 08:03:47 +0700

    Element Filter

---
+++ filter.js
@@ -0,0 +1,18 @@
+const category = document.querySelectorAll(".element_category button");
+const filterElement = document.querySelectorAll('.filterable .character');
+
+
+const filterCategory = e => {
+    document.querySelector('.active').classList.remove('active');
+    e.target.classList.add("active");
+    console.log(e.target);
+
+    filterElement.forEach(character => {
+        character.classList.add("hide");
+
+        if(character.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
+            character.classList.remove("hide");
+        }
+    })
+}
+category.forEach(button => button.addEventListener('click', filterCategory));
